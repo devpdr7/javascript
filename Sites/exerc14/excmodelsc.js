ca6de0565e53d0c2efc0f1bd23d337925a1b31d6
devpdr7 <168119671+devpdr7@users.noreply.github.com>
2024-05-11 21:53:23 -0300

    Atualização no Relógio Automático

--- Sites/exerc14/excmodelsc.js
+++ Sites/exerc14/excmodelsc.js
@@ -2,17 +2,22 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora = data.getHours() 
+    var recep = window.document.getElementById('recep')
     msg.innerHTML = `Agora são ${hora} horas.`
+    recep.innerHTML = ""
     if (hora >= 6 && hora <= 12) {
         img.src = 'fotomanha.png'
         document.body.style.background = '#fdddad'
+        recep.innerHTML = `Bom dia!`
     } else if (hora <=18 && hora >=13) {
         img.src = 'fototarde.png'
         document.body.style.background = '#ce8a55'
+        recep.innerHTML = `Boa tarde!`
     } else {
         img.src = 'fotonoite.png'
         document.body.style.background = '#161c36'
+        recep.innerHTML = `Boa noite!`
     }
     
 }
